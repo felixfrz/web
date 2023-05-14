5ab9e07491f2537df535e5700a872bd17d7c4be3
frz <frz@frzs-MacBook-Pro.local>
2023-05-14 14:43:54 +0100

    First Component

--- react-basic/my-app/src/index.js
+++ react-basic/my-app/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom/client';
+// The first two lines import necessary parts of React. Think of it as bringing in tools that we need to use.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+	return <h2>First component</h2>;
+}
+// The code then defines a function called "Greeting". This function is like a blueprint for a specific part of the website or application that we want to create. In this case, it represents a heading saying "First component".
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDom.createRoot(document.getElementById('root'));
+// Next, there is a line that creates a special object called "root". This object is used by React to manage the rendering of our components on the webpage.
+
+root.render(<Greeting />);
+// In simpler terms, this code sets up a basic React application that displays a heading saying "First component" on a webpage.
