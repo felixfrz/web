ffeaffee8b1b72fcfc22cf0986248756b5a8cf0c
frz <frz@frzs-MacBook-Pro.local>
2023-05-17 18:15:58 +0100

    components

--- react-basic/my-app/src/index.js
+++ react-basic/my-app/src/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-// The first two lines import necessary parts of React. Think of it as bringing in tools that we need to use.
+import './index.css'
 
-function Greeting() {
-	return <h2>First component</h2>;
-}
-// The code then defines a function called "Greeting". This function is like a blueprint for a specific part of the website or application that we want to create. In this case, it represents a heading saying "First component".
+const BookList = () => {
+	return (
+		<section className='booklist'>
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+		</section>
+	);
+};
 
+const Book = () => {
+	return (
+		<article className='book'>
+			<Image />
+			<Title />
+			<Author />
+		</article>
+	);
+};
+
+const Image = () => {
+	return (
+		<img
+			src='https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg'
+			alt='Happy Place'
+		/>
+	);
+};
+const Author = () => {
+	return <p>Emily Henry</p>;
+};
+const Title = () => {
+	return <h2>Happy Place</h2>;
+};
 const root = ReactDom.createRoot(document.getElementById('root'));
-// Next, there is a line that creates a special object called "root". This object is used by React to manage the rendering of our components on the webpage.
 
-root.render(<Greeting />);
-// In simpler terms, this code sets up a basic React application that displays a heading saying "First component" on a webpage.
+root.render(<BookList />);
